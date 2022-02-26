bdad47bbd75f6566a95106e549bb1905b1e8b262
Naval Hurpade <naval@sorigin.com>
2022-02-27 00:02:05 +0530

    added prerendering for post page

--- pages/[username]/[slug].jsx
+++ pages/[username]/[slug].jsx
@@ -1,7 +1,69 @@
 import React from 'react';
+import {
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  addDoc,
+  getDocs,
+  query,
+  collectionGroup,
+  DocumentReference,
+  where,
+} from 'firebase/firestore';
+import { firestore } from '../../lib/firebase';
+import { getUserByUsername, postToJSON } from '../../lib/dbInteraction';
 
-function slug(props) {
-  return <div></div>;
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserByUsername(username);
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = collection(userDoc.ref, `/posts`);
+    const postQuery = query(postRef, where('slug', '==', slug));
+
+    post = postToJSON((await getDocs(postQuery)).docs[0]);
+    path = postRef.path;
+  }
+
+  return {
+    props: {
+      post,
+      path,
+    },
+    revalidate: 5000,
+  };
 }
 
-export default slug;
+export async function getStaticPaths() {
+  let collRef = collectionGroup(firestore, 'posts');
+  let postQuery = query(collRef);
+
+  let paths = (await getDocs(postQuery)).docs.map(postToJSON).map((p) => {
+    return {
+      params: {
+        username: p.userName.toString(),
+        slug: p.slug.toString(),
+      },
+    };
+  });
+
+  /**
+   * return format must be
+   *  path: [
+   *   { params: {userName, slug} }
+   *  ]
+   */
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+}
+
+const PostDetailed = ({ post, path }) => {
+  return <div>sd</div>;
+};
+
+export default PostDetailed;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,24 @@
 import { async } from '@firebase/util';
-import { collectionGroup, getDocs, limit, orderBy, query, where } from 'firebase/firestore';
+import { collectionGroup, getDocs, limit, orderBy, query, where, startAt, Timestamp } from 'firebase/firestore';
 import Head from 'next/head';
 import Image from 'next/image';
 import { postToJSON } from '../lib/dbInteraction';
 import { firestore } from '../lib/firebase';
 import styles from '../styles/Home.module.css';
-
+import Postfeed from './../components/Postfeed';
+import Loader from './../components/Loader';
+import { useState } from 'react';
 const LIMIT = 1;
 
 export async function getServerSideProps() {
   try {
     let collRef = collectionGroup(firestore, 'posts');
-    let postQuery = query(collRef, where('plublished', '==', true), orderBy('createdAt', 'desc'), limit(LIMIT));
+    let postQuery = query(collRef);
 
-    let posts = (await getDocs(postQuery)).docs.map(postToJSON);
-    console.log('POSTS:: ', posts);
+    let initialPosts = (await getDocs(postQuery)).docs.map(postToJSON);
     return {
       props: {
-        posts,
+        initialPosts,
       },
     };
   } catch (error) {
@@ -25,6 +26,44 @@ export async function getServerSideProps() {
   }
 }
 
-export default function Home({ posts }) {
-  return <div>Home</div>;
+export default function Home({ initialPosts }) {
+  const [posts, setPosts] = useState(initialPosts);
+  const [loading, setLoading] = useState(false);
+  const [reachedToEnd, setReachedToEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+
+    const lastPost = posts[posts.length - 1];
+
+    const lastPostAtTime =
+      typeof lastPost.createdAt === 'number' ? Timestamp.fromMillis(lastPost.createdAt) : lastPost.createdAt;
+
+    let collRef = collectionGroup(firestore, 'posts');
+    let postQuery = query(
+      collRef,
+      where('plublished', '==', true),
+      orderBy('createdAt', 'desc'),
+      startAt(lastPostAtTime),
+      limit(LIMIT)
+    );
+
+    let morePosts = (await getDocs(postQuery)).docs.map(postToJSON);
+
+    setPosts(posts.concat(morePosts));
+    setLoading(false);
+
+    if (morePosts.length < LIMIT) {
+      setReachedToEnd(true);
+    }
+  };
+
+  return (
+    <div>
+      <Postfeed posts={posts} />
+      {!reachedToEnd && <button onClick={getMorePosts}>Load more</button>}
+      <Loader show={loading} />
+      {reachedToEnd && 'you reached end of page'}
+    </div>
+  );
 }
